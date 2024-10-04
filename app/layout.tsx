9defab5148a379f76a2c736c44e131c2e1f96f69
Arion Almond <arionalmond@arions-mbp.lan>
2024-10-04 14:17:43 -0400

    good for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,39 @@
+/*
+Good to know: 
+  You should not manually add <head> tags such as <title> and <meta> to root layouts. 
+  Instead, use the Metadata API which automatically handles advanced requirements such as streaming and de-duplicating <head> elements.
+*/
+
+import Link from 'next/link'
+
+// These styles apply to every route in the application
+import './global.css'
+
+// external css
+// import 'bootstrap/dist/css/bootstrap.css'
+// https://nextjs.org/docs/app/building-your-application/styling/css#external-stylesheets
+
 export default function RootLayout({
 
-    children,
-  
+  children,
+
 }: {
 
-    children: React.ReactNode
+  children: React.ReactNode
 
 }) {
 
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
+  return (
+    <html lang="en">
+      {/* Layout UI */}
+      <body>
+        <h3>Navigation</h3>
+        {/* <ol> */}
+        {/* <p><Link href="/">Home</Link></p> */}
+        {/* </ol> */}
+        {children}
+      </body>
+    </html>
+  )
 
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+// http://localhost:3000/a
+
+/*
+Good to know:
+
+The .js, .jsx, or .tsx file extensions can be used for Pages.
+A page is always the leaf of the route subtree.
+A page.js file is required to make a route segment publicly accessible.
+Pages are Server Components by default, but can be set to a Client Component.
+Pages can fetch data. View the Data Fetching section for more information. 
+*/
+
+
+import type { Metadata } from 'next'
+import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'home',
+}
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+  return <div>
+    <br></br>
+    <br></br>
+    
+    <a href='https://arionalmond.github.io/blog/' target="_blank">Blog</a>
+    <br></br>
+    <br></br>
+    <a href='https://arion.serveuser.com/' target="_blank">User Server</a>
+    <br></br>
+    <br></br>
+    <a href='https://arion.moneyhome.biz/' target="_blank">Business Server</a>
+  </div>
+}
+
